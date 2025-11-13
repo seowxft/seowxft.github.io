a8ccc75042e2707e80611b6bdb10b87703aacf20
Tricia Seow <62999342+seowxft@users.noreply.github.com>
2025-11-13 20:42:02 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-caa4b9a2033f8d03ed0d.js"
+    "url": "webpack-runtime-71a39e4311a8d7814374.js"
   },
   {
     "url": "framework-e4c0a6f2e565066ab433.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "ebc913227e149e6a905e686e4a6c6019"
+    "revision": "b7f968a9d4282209ba24cfe478905775"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-3528d06e642fb632834b.js"
